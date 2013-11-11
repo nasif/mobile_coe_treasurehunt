fee1a374fd0bb4968c29e146171c8e7ebde8f61e
paikrao.mahadev <nnd.ravindra@gmail.com>
2013-11-11 14:52:34 +0530

    intial commit from paikrao

--- src/com/tavant/mobilecoe/treasurehunt/ui/HelpActivity.java
+++ src/com/tavant/mobilecoe/treasurehunt/ui/HelpActivity.java
@@ -11,7 +11,7 @@ public class HelpActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.scan_layout);
+		
 	}
 	@Override
 	protected void onStart() {
