84a609a4ce5310a683714f393fb6db02e07c2b74
paikrao.mahadev <nnd.ravindra@gmail.com>
2013-11-11 16:16:18 +0530

    added help layout and string xml

--- src/com/tavant/mobilecoe/treasurehunt/ui/HelpActivity.java
+++ src/com/tavant/mobilecoe/treasurehunt/ui/HelpActivity.java
@@ -11,6 +11,7 @@ public class HelpActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
 		
 	}
 	@Override
